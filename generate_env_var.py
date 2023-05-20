e5000226f43e0d401641e2573910ec62bd1087a8
1061018 <75098493+1061018@users.noreply.github.com>
2023-05-20 17:43:07 +0100

    Update generate_env_var.py

--- generate_env_var.py
+++ generate_env_var.py
@@ -1,5 +1,6 @@
 import os
 
-host = "MY_SECRET"
-with open(os.environ["GITHUB_ENV"], "a") as env_file:
-    env_file.write(f"host={host}")
+env_file = os.getenv('GITHUB_ENV')
+
+with open(env_file, "a") as myfile:
+    myfile.write("host=MY_SECRET")
